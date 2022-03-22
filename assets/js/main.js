46a2851ee00650f90f2f845dd191c31113e4c19e
Pierre-Yves DELETTRE <dev.py@delettre.me>
2022-03-22 01:17:48 +0100

    fix default theme
    if no theme is stored already, use the light as default

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,42 @@
-//TODO github ci actions to remove comments, lint...
-
 // dark/light theme manager
-// bg and text colors variables are stored in 2 SCSS classes .theme-light and .theme-dark
-// apply one class to the <html> element to set the theme
+// bg and text colors are stored in 2 CSS classes : .theme-light and .theme-dark
+// theme is set by adding one class to the <html> element
 const theme = {
-  // element : ball moving right left to mask sun or moon in the switch
-  ball : null,
-  // initialize : 
+  // click on the switch triggers the ball translation
+  ballEl : null,
+  // current theme key in local storage
+  key : 'theme',
+  // stored value for dark theme
+  darkValue : 'theme-dark',
+  // stored value for light theme
+  lightValue : 'theme-light',
   init : function() {
-    this.ball = document.querySelector('#switch-container .switch .ball');
-    this.set(localStorage.getItem('theme'));
-    },
-    /**
-     * set the theme of the site
-     * also store current theme name : 
-     * @param {*} name 
-     */
-    set : function(name) {
-      localStorage.setItem('theme', name);
-      document.documentElement.className = name;
-    },
-    // switch theme
-    toggle: function() {
-      
-      if (localStorage.getItem('theme') === 'theme-dark') {
-        this.set('theme-light');
-        this.ball.classList.remove('darkMode');
-      } else {
-        this.set('theme-dark');
-        this.ball.classList.add('darkMode');
-      }
-    },
+    this.ballEl = document.querySelector('#switch-container .switch .ball');
+    // set theme according to its stored name or default to light
+    const currentTheme = localStorage.getItem('theme');
+    currentTheme === null ? this.set(this.lightValue) : this.set(currentTheme);
+  },
+  // set theme : store theme name and update CSS
+  set : function(name) {
+    localStorage.setItem('theme', name);
+    document.documentElement.className = name;
+  },
+  // current stored theme is the dark one
+  isDark : function() {
+    return localStorage.getItem('theme') === this.darkValue;
+  },
+  toggle: function() {
+    this.isDark() ? this.set(this.lightValue) : this.set(this.darkValue);
+    this.ballEl.classList.toggle('darkMode');
   }
+}
   
-// the whoami element (half of the home page about me)
-// click on this element will toggle content between identity and profile
+// the whoami element
+// when clicked toggles between identity and profile content
 const whoami = {
   el : null,
   identityEl : null,
   profileEl : null,
-  // initialize
   init : () => {
     this.el = document.querySelector('#home .whoami');
     this.identityEl = this.el.querySelector('.identity');
@@ -72,6 +69,5 @@ const app = {
 }
 
 app.init();
-console.log('page name', app.page);
 
 
